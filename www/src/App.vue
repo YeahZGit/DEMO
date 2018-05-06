ffdbf5a01f7973fd4a5f4cf0906180ced794f371
YeahZ <1259747963@qq.com>
2018-05-06 22:38:11 +0800

    initial commit

---
+++ www/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <tabbar></tabbar>
+  </div>
+</template>
+
+<script>
+import Tabbar from './components/common/Tabbar.vue';
+export default {
+  name: 'App',
+  components: {
+    Tabbar
+  }
+}
+</script>
+
+<style>
+</style>
